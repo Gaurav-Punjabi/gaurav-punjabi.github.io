652229821f6205188fa52e14622e4e428cd4d31c
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-01-03 23:45:59 +0530

    Changed the color scheme

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@
     const gNavItems                     =   document.querySelectorAll('.global-menu__item');
     const elmOverlay                    =   document.querySelector('.shape-overlays');
     const overlay                       =   new ShapeOverlays(elmOverlay);
+    const coverImage                    =   $(".cover-image");
     let currentPage                     =   $('#home');
     let menuLinks                       =   $('.menu-link');
     $('section').hide();
     currentPage.show();
+    coverImage.hide();
 
 
     elmHamburger.addEventListener('click', () => {
@@ -16,12 +18,13 @@
         overlay.toggle();
         if (overlay.isOpened === true) {
             elmHamburger.classList.add('is-opened-navi');
+            // setTimeout(() => coverImage.show(), 750);
             for (var i = 0; i < gNavItems.length; i++) {
                 gNavItems[i].classList.add('is-opened');
             }
         } else {
             elmHamburger.classList.remove('is-opened-navi');
-
+            coverImage.hide();
             for (var i = 0; i < gNavItems.length; i++) {
                 gNavItems[i].classList.remove('is-opened');
             }
