2a651c34d40125dc05bf13ddf46a7c3c035d0b71
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-01-06 03:41:54 +0530

    Adding about-me page

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@
     const elmOverlay                    =   document.querySelector('.shape-overlays');
     const overlay                       =   new ShapeOverlays(elmOverlay);
     const coverImage                    =   $(".cover-image");
-    let currentPage                     =   $('#home');
+    let currentPage                     =   $('#about');
     let menuLinks                       =   $('.menu-link');
+    let prevLink                        =   currentPage;
+    prevLink.addClass("active");
     $('section').hide();
     currentPage.show();
     coverImage.hide();
@@ -31,24 +33,27 @@
         }
     });
 
-    // menuLinks.click(function() {
-    //     if (overlay.isAnimating) {
-    //         return false;
-    //     }
-    //     overlay.toggle();
-    //
-    //     elmHamburger.classList.remove('is-opened-navi');
-    //
-    //     let requestedPage = $(this).attr('id');
-    //     console.log(`Requested page : ${requestedPage}`);
-    //     requestedPage = $(requestedPage);
-    //
-    //     for (var i = 0; i < gNavItems.length; i++) {
-    //         gNavItems[i].classList.remove('is-opened');
-    //     }
-    //     currentPage.hide();
-    //     requestedPage.show();
-    //     requestedPage.addClass("animated").addClass("slideInDown");
-    //     currentPage = requestedPage;
-    // })
+    menuLinks.click(function() {
+        if (overlay.isAnimating) {
+            return false;
+        }
+        overlay.toggle();
+
+        elmHamburger.classList.remove('is-opened-navi');
+
+        let requestedPage = $(this).attr('id');
+        console.log(`Requested page : ${requestedPage}`);
+        requestedPage = $(requestedPage);
+
+        for (var i = 0; i < gNavItems.length; i++) {
+            gNavItems[i].classList.remove('is-opened');
+        }
+        currentPage.hide();
+        requestedPage.show();
+        requestedPage.addClass("animated").addClass("slideInDown");
+        prevLink.removeClass("active");
+        prevLink = $(this);
+        prevLink.addClass("active");
+        currentPage = requestedPage;
+    })
 }());
